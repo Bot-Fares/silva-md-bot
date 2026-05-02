69c2ee248708223be52b4d436b2b982c3384cec0
Bot-Fares <rawyfares2@gmail.com>
2026-05-02 16:24:32 +0300

    Refactor rkick command to use async handler

--- plugins/Kickr.js
+++ plugins/Kickr.js
@@ -1,22 +1,23 @@
-// امر الطرد العشوائي لبوت سيلفا
-case 'rkick': {
+const handler = async (m, { sock, isGroup, isBotAdmin, isAdmin, reply }) => {
     if (!isGroup) return reply('الأمر ده للجروبات بس يا فارس!');
-    if (!isBotAdmin) return reply('لازم ترفع البوت آدمن الأول عشان يطرد!');
     if (!isAdmin) return reply('الأمر ده للمشرفين بس!');
+    if (!isBotAdmin) return reply('لازم ترفع البوت آدمن الأول عشان يطرد!');
 
-    // جلب قائمة الأعضاء
-    const groupMetadata = await sock.groupMetadata(from);
+    const groupMetadata = await sock.groupMetadata(m.chat);
     const participants = groupMetadata.participants;
-
-    // تصفية القائمة لاستبعاد الآدمن والبوت نفسه
     const victims = participants.filter(p => !p.admin && p.id !== sock.user.id);
 
     if (victims.length === 0) return reply('الجروب كله آدمن يا بطل، مفيش حد أطرده!');
 
-    // اختيار شخص عشوائي (قانون الاحتمالات)
     const randomVictim = victims[Math.floor(Math.random() * victims.length)].id;
 
-    reply(`وقع الاختيار العشوائي على: @${randomVictim.split('@')[0]}.. مع السلامة!`);
-    await sock.groupParticipantsUpdate(from, [randomVictim], 'remove');
-}
-break;
+    reply(`وقع الاختيار العشوائي على: @${randomVictim.split('@')[0]}.. مع السلامة!`, m.chat, { mentions: [randomVictim] });
+    await sock.groupParticipantsUpdate(m.chat, [randomVictim], 'remove');
+};
+
+handler.command = ['rkick'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+module.exports = handler;
